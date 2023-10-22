01623dd6aed63d99974dba19c54c42e82bea8275
yref-boop <regofdez.yago@gmail.com>
2023-10-22 19:37:46 +0200

    chore: added gitignore

---
+++ lambda/parser.mli
@@ -0,0 +1,26 @@
+type token =
+  | LAMBDA
+  | TRUE
+  | FALSE
+  | IF
+  | THEN
+  | ELSE
+  | SUCC
+  | PRED
+  | ISZERO
+  | LET
+  | IN
+  | BOOL
+  | NAT
+  | LPAREN
+  | RPAREN
+  | DOT
+  | EQ
+  | COLON
+  | ARROW
+  | EOF
+  | INTV of (int)
+  | IDV of (string)
+
+val s :
+  (Lexing.lexbuf  -> token) -> Lexing.lexbuf -> Lambda.term
